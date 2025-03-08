4035520133f16f9139f590ec3b817765ee9db89c
roberto <rvillegas@outlook.com>
2025-03-07 22:17:33 -0300

    zombie update

--- POO_test/zombie.py
+++ POO_test/zombie.py
@@ -1,9 +1,22 @@
-from enemy import *
+from Enemy import *
+import random
+
 
 class Zombie(Enemy):
     
-    def __init__(self,type_of_enemy ,healt_points, attack_damage):
-        super().__init__(type_of_enemy=type_of_enemy,
+    def __init__(self,healt_points, attack_damage):
+        super().__init__(type_of_enemy='Zombie',
                          health_points=healt_points,
                          attack_damage=attack_damage)
-        
+    
+    def talk(self):
+        print("*Grumbling...*")
+
+    def spread_disease(self):
+        print("The zombie is trying to spread infection")
+    
+    def special_attack(self):
+        did_special_attack_works = random.random() < 0.50
+        if did_special_attack_works:
+              self.health_points += 2
+              print('Zombie regenerated 2HP!')
